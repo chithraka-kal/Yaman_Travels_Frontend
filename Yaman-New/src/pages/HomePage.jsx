f73b3e3cc9203fb44176e49303759bdbed504d34
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-05-28 15:41:36 +0530

    carousel

--- Yaman-New/src/pages/HomePage.jsx
+++ Yaman-New/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar.jsx";
-import Carousel from "../components/Carousel.jsx";
+import Carousel from "../components/CarouselWithContent.jsx";
 
 function HomePage() {
     return (
