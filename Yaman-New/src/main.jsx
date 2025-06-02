abfebb4253df694a7936d4590a33936939adff3b
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-06-03 00:28:59 +0530

    main

--- Yaman-New/src/main.jsx
+++ Yaman-New/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'flowbite'; 
-import App from './app/App.jsx'
+import App from './App/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
