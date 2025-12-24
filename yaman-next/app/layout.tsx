0bbca23f2296115a484be379dc69d054bccf766a
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-12-24 12:06:28 -0800

    layout

--- yaman-next/app/layout.tsx
+++ yaman-next/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 // Note: If 'components' is at the root, '../components' is correct.
 import Navbar from "../components/Navbar"; 
 import Footer from "../components/Footer";
+import AuthProvider from "../components/AuthProvider";
 
 export const metadata = {
   title: "Yaman Travels",
@@ -14,9 +15,11 @@ export default function RootLayout({children,}: {children: React.ReactNode;
   return (
     <html lang="en">
       <body className="antialiased">
+        <AuthProvider>
         <Navbar />
         <main>{children}</main>
         <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
