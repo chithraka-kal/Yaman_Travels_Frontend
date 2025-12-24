ee42cb190736825a7973f90f40de6930e95f5001
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-12-24 11:36:43 -0800

    route

---
+++ yaman-next/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+
+      async authorize(credentials) {
+        const { username, password } = credentials; // Note: Our login form uses 'username' field to send email usually, or we change login form to send 'email'
+
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email: username }); // Find by email
+
+          if (!user) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (!passwordsMatch) {
+            return null;
+          }
+
+          return user;
+        } catch (error) {
+          console.log("Error: ", error);
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export { handler as GET, handler as POST };
